6b577148ecbc9972e84256306f8153f087b74dbb
limasuu <szlima.93@gmail.com>
2022-06-01 14:05:28 -0300

    Criando a função definirAcertos

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const definirParidade= () => Math.random()<0.5 ? true : false;
 const avaliarParidade= (num, par) => par ? ((num%2===0) ? true : false) 
             : ((num%2 ===0) ? false : true);
 
+const definirAcertos= () => {
+    let acertos= 0;
+
+    return {
+        add: () => acertos++,
+        getTotal: () => acertos        
+    };
+};
